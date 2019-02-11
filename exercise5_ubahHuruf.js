9cd29af1dd3d6a8bd8c4280d1a9fed6ce8c70c20
ASUS <hutasoitnoel@gmail.comgit config --global user.name noel>
2019-02-11 18:19:56 +0700

    exercise5

---
+++ exercise5_ubahHuruf.js
@@ -0,0 +1,21 @@
+function ubahHuruf(kata) {
+  var huruf = "abcdefghijklmnopqrstuvwxyz";
+  var kataSplit = kata.split("");
+
+  for (var i = huruf.length ; i >= 0 ; i--){
+    for (var j = 0; j < kataSplit.length ; j++){
+      if (kataSplit[j] === huruf[i]){
+        kataSplit[j] = huruf[i+1];
+      }
+    }
+  }
+
+  return kataSplit.join("");
+}
+
+// TEST CASES
+console.log(ubahHuruf('wow')); // xpx
+console.log(ubahHuruf('developer')); // efwfmpqfs
+console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+console.log(ubahHuruf('keren')); // lfsfo
+console.log(ubahHuruf('semangat')); // tfnbohbu
